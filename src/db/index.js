93a65adfe115f3e7cd3712e8f9122a80a7791777
K RAJA <cse.raja007@gmail.com>
2024-05-24 09:12:37 +0530

    connected to mongoDB

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../contants.js";
+
+const connectDB = async () => {
+  const connectionInstance = await mongoose.connect(
+    `${process.env.MONGO_URL}/${DB_NAME}`
+  );
+
+  console.log(
+    `\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`
+  );
+  try {
+  } catch (error) {
+    console.log("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
